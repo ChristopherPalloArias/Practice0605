bad716ece4931776d2c52682d931da91f6ba19ba
ChristopherPalloArias <cipallo@uce.edu.ec>
2024-05-06 11:37:29 -0500

    feat: :white_check_mark: Add new functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Login from './Login'
+import React, { useState } from 'react';
+import Login from './Login';
+import HomePage from './HomePage'; // Importa el componente HomePage
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLoginSuccess() {
+    setIsLoggedIn(true);
+  }
+
   return (
     <div>
-      <Login/>
+      {isLoggedIn ? <HomePage /> : <Login onLoginSuccess={handleLoginSuccess} />}
     </div>
-  )
+  );
 }
 
 export default App;
